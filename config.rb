946f66b8a7e2ab06d8c99a28f1ff6b2ae96bb9dd
Nadhim Orfali <info@fenka.co.uk>
2013-02-13 17:19:33 +0000

    updated ruby helpers to include rapid checkbox listing plus improvements

--- config.rb
+++ config.rb
@@ -57,7 +57,7 @@ helpers do
 
   # ----------------------------
   # Render textbox
-  def form_textbox(id="", label="Label", placeholder="Enter text", required=false, textboxtype="text", errormsg="This field must be filled in", accesskey="")
+  def form_textbox(id="", label="Label", placeholder="Enter text", required=false, errormsg="This field must be filled in", accesskey="")
     haml_concat <<-"HTML".gsub( /^\s+/, '' )
       <div class="field">
     HTML
@@ -77,23 +77,62 @@ helpers do
 
     if required == true # add a required * if necessary
     haml_concat <<-"HTML".gsub( /^\s+/, '' )
-      <span title="Required">*</span>
+      <span class="field-required" title="Required">*</span>
     HTML
     end
     haml_concat <<-"HTML".gsub( /^\s+/, '' )
         </label>
     HTML
 
-    # render input textbox or textarea
-    if textboxtype == "textarea"
+    haml_concat <<-"HTML".gsub( /^\s+/, '' )
+        <input id="#{id}" name="#{id}" type="text" placeholder="#{placeholder}" class="field-textbox" />
+    HTML
+
+    # error message if required=true
+    if required == true
+    haml_concat <<-"HTML".gsub( /^\s+/, '' )
+      <span class="field-error">#{errormsg}</span>
+    HTML
+    end
+
+    # finish - wrapping up now
+    haml_concat <<-"HTML".gsub( /^\s+/, '' )
+      </div>
+    HTML
+  end
+
+  # ----------------------------
+  # Render textarea
+  def form_textarea(id="", label="Label", placeholder="Enter text", required=false, errormsg="This field must be filled in", accesskey="")
+    haml_concat <<-"HTML".gsub( /^\s+/, '' )
+      <div class="field">
+    HTML
+
+    if accesskey != "" # add accesskey to label if necessary
+      haml_concat <<-"HTML".gsub( /^\s+/, '' )
+          <label for="#{id}" accesskey="#{accesskey}">
+      HTML
+    else
+    haml_concat <<-"HTML".gsub( /^\s+/, '' )
+        <label for="#{id}">
+    HTML
+    end
+    haml_concat <<-"HTML".gsub( /^\s+/, '' )
+        #{label}
+    HTML
+
+    if required == true # add a required * if necessary
+    haml_concat <<-"HTML".gsub( /^\s+/, '' )
+      <span class="field-required" title="Required">*</span>
+    HTML
+    end
+    haml_concat <<-"HTML".gsub( /^\s+/, '' )
+        </label>
+    HTML
+
     haml_concat <<-"HTML".gsub( /^\s+/, '' )
       <textarea id="#{id}" name="#{id}" placeholder="#{placeholder}" class="field-textbox"></textarea>
     HTML
-    else
-    haml_concat <<-"HTML".gsub( /^\s+/, '' )
-        <input id="#{id}" name="#{id}" type="#{textboxtype}" placeholder="#{placeholder}" class="field-textbox" />
-    HTML
-    end
 
     # error message if required=true
     if required == true
@@ -112,8 +151,8 @@ helpers do
   # Render checkbox
   def form_checkbox(id="", label="Label")
     haml_concat <<-"HTML".gsub( /^\s+/, '' )
-      <div class="field field-checkbox">
-        <input id="#{id}" name="#{id}" type="checkbox" class="field-checkbox" />
+      <div class="field">
+        <input id="#{id}" name="#{id}" type="checkbox" />
         <label for="#{id}">
           #{label}
         </label>
@@ -127,6 +166,87 @@ helpers do
   end
 
   # ----------------------------
+  # Render checkboxList
+  def form_checkboxlist(num=3, legendName="Checkbox List", required=false)
+    haml_concat <<-"HTML".gsub( /^\s+/, '' )
+      <fieldset class="field-checkboxlist">
+        <legend>
+          <span>
+            #{legendName}
+    HTML
+    if required == true # add a required * if necessary
+    haml_concat <<-"HTML".gsub( /^\s+/, '' )
+      <span class="field-required" title="Required">*</span>
+    HTML
+    end
+    haml_concat <<-"HTML".gsub( /^\s+/, '' )
+          </span>
+        </legend>
+    HTML
+    count = 1
+    while count <= num
+      form_checkbox("checkbox_"+count.to_s, "checkbox #"+count.to_s)
+      count += 1
+    end
+
+    haml_concat <<-"HTML".gsub( /^\s+/, '' )
+      </fieldset>
+    HTML
+
+  end
+
+  # ----------------------------
+  # Render radio buttons - not used directly
+  def form_radio(id="", label="Label", groupName="")
+    haml_concat <<-"HTML".gsub( /^\s+/, '' )
+      <div class="field">
+        <input id="#{id}" name="#{groupName}" type="radio" />
+        <label for="#{id}">
+          #{label}
+        </label>
+    HTML
+
+    # finish - wrapping up now
+    haml_concat <<-"HTML".gsub( /^\s+/, '' )
+      </div>
+    HTML
+
+  end
+
+  # ----------------------------
+  # Render radio buttons List
+  def form_radiolist(num=3, legendName="Radio List", required=false)
+    haml_concat <<-"HTML".gsub( /^\s+/, '' )
+      <fieldset class="field-radiolist">
+        <legend>
+          <span class="legend">
+            #{legendName}
+    HTML
+    if required == true # add a required * if necessary
+    haml_concat <<-"HTML".gsub( /^\s+/, '' )
+      <span class="field-required" title="Required">*</span>
+    HTML
+    end
+    haml_concat <<-"HTML".gsub( /^\s+/, '' )
+          </span>
+        </legend>
+    HTML
+    count = 1
+    o =  [('a'..'z'),('A'..'Z')].map{|i| i.to_a}.flatten
+    string  =  (0...50).map{ o[rand(o.length)] }.join
+    while count <= num
+      form_radio("radio_"+count.to_s, "radio #"+count.to_s, string)
+      count += 1
+    end
+
+    haml_concat <<-"HTML".gsub( /^\s+/, '' )
+      </fieldset>
+    HTML
+
+  end
+
+
+  # ----------------------------
   # Render selectbox
   def form_selectbox(id="", label="Label", required=false, selectboxtype="select", errormsg="Something must be selected")
     haml_concat <<-"HTML".gsub( /^\s+/, '' )
@@ -136,7 +256,7 @@ helpers do
     HTML
     if required == true # add a required * if necessary
     haml_concat <<-"HTML".gsub( /^\s+/, '' )
-      <span title="Required">*</span>
+      <span class="field-required" title="Required">*</span>
     HTML
     end
     haml_concat <<-"HTML".gsub( /^\s+/, '' )
@@ -178,16 +298,10 @@ helpers do
 
   # ----------------------------
   # Render placeholder image
-  def img_placehold(width="200", height="200", text="", background="hexdddddd", forecolour="hex858585", font="Helvetica", fontsize="24")
-    placehold_params = "?form=png&w=#{width}&h=#{height}&f=#{font}&fs=#{fontsize}&icol=#{background}&col=#{forecolour}"
+  def img_placehold(width="200", height="200")
+    placehold_params = "#{width}x#{height}"
     
-    if text == ""
-      placehold_params = placehold_params + "&t=#{width} x #{height}"
-    else
-      placehold_params = placehold_params + "&t=#{text}"
-    end
-    
-    placehold_url = "http://placehold.development2.sequence.co.uk/" + placehold_params
+    placehold_url = "http://www.imgsrc.me/" + placehold_params
 
     haml_concat <<-"HTML".gsub( /^\s+/, '' )
       <img src="#{placehold_url}" width="#{width}" height="#{height}" />
